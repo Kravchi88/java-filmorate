63cecd75cda31b99e64aa4213d0968237fe52b2e
Konstantin Shushkevich <shusha113@yandex.ru>
2025-01-14 21:45:40 +0300

    Added feature add-reviews fix1

--- src/main/java/ru/yandex/practicum/filmorate/dal/user/InMemoryUserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/user/InMemoryUserStorage.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.dal.user;
 
-import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.User;
 
